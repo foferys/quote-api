69073b0069fafbb7109ed62e1769072823494757
foferys <foferys@gmail.com>
2024-12-14 15:35:04 +0100

    built front-end api docs; deleted tailwind ref

--- src/main/java/com/prova_api/controller/HomeController.java
+++ src/main/java/com/prova_api/controller/HomeController.java
@@ -1,19 +1,40 @@
 package com.prova_api.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.prova_api.phrases.Phrase;
+import com.prova_api.services.PhraseRepository;
 
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home(Model model) {
+    @Autowired
+    private PhraseRepository pRepository;
 
-        
-        String frase = "Frasi simpatiche sulla programmazione";
-        model.addAttribute("frase", frase);
+    @GetMapping("/")
+    public String home(Model model) throws JsonProcessingException {
+
+        Phrase randomphrase = pRepository.findRandomPhrase();
+        model.addAttribute("randomexample", randomphrase.getPhrase());
+
+
+        Map<String, Phrase> response = new HashMap<>();
+        response.put("data", randomphrase);
+
+        // Converti la mappa in una stringa JSON
+        ObjectMapper randomObjectMapper = new ObjectMapper();
+        String randomjsonquote = randomObjectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(response);
+
+        model.addAttribute("randomjson", randomjsonquote);
 
         return "index";
     }
